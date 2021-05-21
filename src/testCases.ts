196e189dc85d4ec2d818fdb49a9ffcee06b89181
Shakhboz Khalikov <khalikov.shakhboz@payme.uz>
2021-05-21 16:55:50 +0500

    CHECK PR

--- src/testCases.ts
+++ src/testCases.ts
@@ -1,47 +1,51 @@
 import { delay, cpuAverage } from "./utils";
-import { ItestObject, IperiodMeasurement, IsetCounters } from "../interface/interfaces";
+import {
+  ItestObject,
+  IperiodMeasurement,
+  IsetCounters,
+} from "../interface/interfaces";
 
 export function runWarmingUp(test: ItestObject, iterations: number) {
   for (let i = 0; i <= iterations; i++) {
     test.getTest();
   }
-  console.log('Прогрев завершен. Выполнение тестов...')
+  console.log("Прогрев завершен. Выполнение тестов...");
 }
 
-export function runBench(tests : ItestObject[], iterations: number, repeats: number) {
-  for(const test of tests){
+export function runBench(
+  tests: ItestObject[],
+  iterations: number,
+  repeats: number
+) {
+  for (const test of tests) {
     getTestsBench(test, iterations, repeats);
   }
 }
 
-function getTestsBench(
-  test: ItestObject,
-  iterations: number,
-  repeats: number
-) {
-  let counters = setCounters()
-    for (let i = 0; i < repeats; i++) {
-      const startMeasure = periodMeasurement();
-      for (let i = 0; i <= iterations; i++) {
-        test.getTest();
-        counters.testTitle = test.name;
-      }
-      const endMeasure = periodMeasurement();
-      counters.counterTime += getUsedTime(endMeasure.time, startMeasure.time);
-      counters.counterMemoryUsage += getUsedMemory();
-      counters.counterCpu += getCPUPercentage(endMeasure, startMeasure);
-      delay(500).then();
+function getTestsBench(test: ItestObject, iterations: number, repeats: number) {
+  let counters = setCounters();
+  for (let i = 0; i < repeats; i++) {
+    const startMeasure = periodMeasurement();
+    for (let i = 0; i <= iterations; i++) {
+      test.getTest();
+      counters.testTitle = test.name;
+    }
+    const endMeasure = periodMeasurement();
+    counters.counterTime += getUsedTime(endMeasure.time, startMeasure.time);
+    counters.counterMemoryUsage += getUsedMemory();
+    counters.counterCpu += getCPUPercentage(endMeasure, startMeasure);
+    delay(500).then();
   }
-  logInfo(counters, repeats);
+  logInfo(counters, repeats); // хуета
 }
 
-function setCounters(): IsetCounters  {
+function setCounters(): IsetCounters {
   return {
-  counterTime: 0,
-  counterMemoryUsage: 0,
-  counterCpu: 0,
-  testTitle: ""
-  }
+    counterTime: 0,
+    counterMemoryUsage: 0,
+    counterCpu: 0,
+    testTitle: "",
+  };
 }
 
 function getCPUPercentage(
@@ -69,16 +73,19 @@ function periodMeasurement(): IperiodMeasurement {
     cpu: cpuAverage(),
   };
 }
-function logInfo(
-  counters: IsetCounters,
-  repeats: number
-) {
+function logInfo(counters: IsetCounters, repeats: number) {
   console.log(counters.testTitle);
-  console.log("Avg work time: " + (counters.counterTime / repeats).toFixed(2) + " ms");
   console.log(
-    "Avg memory used: " + (counters.counterMemoryUsage / repeats).toFixed(2) + " MB"
+    "Avg work time: " + (counters.counterTime / repeats).toFixed(2) + " ms"
   );
-  console.log("Avg CPU used: " + (counters.counterCpu / repeats).toFixed(2) + " %");
+  console.log(
+    "Avg memory used: " +
+      (counters.counterMemoryUsage / repeats).toFixed(2) +
+      " MB"
+  );
+  console.log(
+    "Avg CPU used: " + (counters.counterCpu / repeats).toFixed(2) + " %"
+  );
   console.log(
     "---------------------------------------------------------------"
   );
